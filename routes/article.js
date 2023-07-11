3a26517ac3931625e47ff9e1d7613f9ef6c5a007
Drozdzik2137 <79859864+Drozdzik2137@users.noreply.github.com>
2023-07-11 18:47:38 +0200

    routes for new controller

--- routes/article.js
+++ routes/article.js
@@ -4,44 +4,52 @@ const secure = require('../middleware/authMiddleware');
 const path = require('path');
 const multer = require('multer');
 const fs = require('fs');
+const articleController = require('../controllers/articleController');
 
+// helpers to save files on the server
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      const today = new Date();
-      const year = today.getFullYear();
-      const month = String(today.getMonth() + 1).padStart(2, '0');
-      const day = String(today.getDate()).padStart(2, '0');
-      const pathImages = `uploads/gallery/${year}-${month}-${day}/`;
-      fs.mkdirSync(pathImages, { recursive: true });
-      const pathThumbnail = `uploads/thumbnail/`;
-      fs.mkdirSync(pathThumbnail, { recursive: true });
-      cb(null, file.fieldname === 'thumbnail' ? pathThumbnail : pathImages);
-    },
-    filename: function (req, file, cb) {
-      // to set a different file name
-      const ext = path.extname(file.originalname);
-      const filename = `${Date.now()}${ext}`;
-      cb(null, filename);
-      // to set original file name
-      // cb(null, file.originalname);
-    }
+  destination: function (req, file, cb) {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    const pathImages = `uploads/gallery/${year}-${month}-${day}/`;
+    fs.mkdirSync(pathImages, { recursive: true });
+    const pathThumbnail = `uploads/thumbnail/`;
+    fs.mkdirSync(pathThumbnail, { recursive: true });
+    cb(null, file.fieldname === 'thumbnail' ? pathThumbnail : pathImages);
+  },
+  filename: function (req, file, cb) {
+    // to set a different file name
+    const ext = path.extname(file.originalname);
+    const filename = `${Date.now()}${ext}`;
+    cb(null, filename);
+    // to set original file name
+    // cb(null, file.originalname);
+  }
 });
 
 const fileFilter = function (req, file, cb) {
-    // Accepted file extensions
-    const allowedExtensions = ['.png', '.jpg', '.jpeg', '.webp', '.gif', '.webm', '.svg'];
-  
-    // Check that the file extension is among the accepted extensions
-    const ext = path.extname(file.originalname).toLowerCase();
-    if (allowedExtensions.includes(ext)) {
-      cb(null, true);
-    } else {
-      cb(new Error('Nieprawidłowe rozszerzenie pliku.'));
-    }
-  };
+  // Accepted file extensions
+  const allowedExtensions = ['.png', '.jpg', '.jpeg', '.webp', '.gif', '.webm', '.svg'];
+
+  // Check that the file extension is among the accepted extensions
+  const ext = path.extname(file.originalname).toLowerCase();
+  if (allowedExtensions.includes(ext)) {
+    cb(null, true);
+  } else {
+    cb(new Error('Nieprawidłowe rozszerzenie pliku.'));
+  }
+};
 
 const upload = multer({ storage: storage, fileFilter: fileFilter });
-const articleController = require('../controllers/articleController');
+
+
+// Get the article - no need JWT auth
+router.route('/article/:id').get(articleController.getArticle);
+
+// Get all articles - no need JWT auth
+router.route('/articles').get(articleController.getArticles);
 
 // Add new article
 router.route('/article').post(secure.authenticateUser, upload.fields([
@@ -59,12 +67,6 @@ router.route('/article/:id').put(secure.authenticateUser, articleController.edit
 // Delete an article
 router.route('/article/:id').delete(secure.authenticateUser, articleController.deleteArtcile);
 
-// Get an article - no need JWT auth
-router.route('/article/:id').get(articleController.getArticle);
-
-// Get all articles - no need JWT auth
-router.route('/articles').get(articleController.getArticles);
-
 // Add images to article
 router.route('/article/:id/images').post(secure.authenticateUser, upload.array('images'), articleController.addImageToArticle);
 
@@ -76,5 +78,17 @@ router.route('/article/:id/images').post(secure.authenticateUser, upload.array('
 
 // Delete an thumbnail (single thumbnail) from an thumbnail
 router.route('/article/:articleId/images/:imageId').delete(secure.authenticateUser, articleController.deleteThumbnailFromArticle);
+
+// Get the article for admin
+router.route('/admin/article/:id').get(secure.authenticateUser, articleController.getArticleForAdmin);
+
+// Get all artciles for admin
+router.route('/admin/articles').get(secure.authenticateUser, articleController.getArticlesForAdmin);
+
+// Change to public
+router.route('/article/:id/public').put(secure.authenticateUser, articleController.changeToPublic);
+
+// Change to private
+router.route('/article/:id/public').put(secure.authenticateUser, articleController.changeToPrivate);
 
 module.exports = router;
